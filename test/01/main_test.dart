05d488be1215f19a32d60d9f008d96a37fc0e379
Cillian Myles <myles.cillian@gmail.com>
2023-12-01 22:29:44 +0000

    chore: add tests for part 2

--- test/01/main_test.dart
+++ test/01/main_test.dart
@@ -1,15 +1,37 @@
 import 'package:advent_of_code/01/main.dart';
 import 'package:test/test.dart';
 
-const _input = '''
+void main() {
+  group('verify samples', () {
+    test('part 1', () {
+      expect(
+        sumOfCalibrationValues(_inputPart1),
+        142,
+      );
+    });
+
+    test('part 2', () {
+      expect(
+        sumOfCalibrationValues(_inputPart2),
+        281,
+      );
+    });
+  });
+}
+
+const _inputPart1 = '''
 1abc2
 pqr3stu8vwx
 a1b2c3d4e5f
 treb7uchet
 ''';
 
-void main() {
-  test('verify sample', () {
-    expect(sumOfCalibrationValues(_input), 142);
-  });
-}
+const _inputPart2 = '''
+two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen
+''';
