02e908dbc28fe43d741865f63fa0ac38234d9ddb
Cillian Myles <myles.cillian@gmail.com>
2023-12-01 22:25:16 +0000

    feat: WIP part 2

--- lib/01/main.dart
+++ lib/01/main.dart
@@ -14,12 +14,33 @@ int sumOfCalibrationValues(String input) {
   for (final line in lines) {
     if (line.isEmpty) continue;
 
-    final numbers =
-        RegExp(r'\d').allMatches(line).map((m) => m.group(0)!).toList();
+    final numbers = RegExp(r'\d|one|two|three|four|five|six|seven|eight|nine')
+        .allMatches(line)
+        .map((m) => m.group(0)!)
+        .toList();
 
-    final value = int.parse('${numbers.first}${numbers.last}');
+    final first =
+        numbers.first.length > 1 ? _wordLookup[numbers.first] : numbers.first;
+    final last =
+        numbers.last.length > 1 ? _wordLookup[numbers.last] : numbers.last;
+
+    final value = int.parse('$first$last');
     sum += value;
+
+    print('$line - $numbers - $first - $last - $value');
   }
 
   return sum;
 }
+
+const _wordLookup = {
+  'one': '1',
+  'two': '2',
+  'three': '3',
+  'four': '4',
+  'five': '5',
+  'six': '6',
+  'seven': '7',
+  'eight': '8',
+  'nine': '9',
+};
