8393fe8ac8b84e8af43a0afe77f58df7c54f9956
Cillian Myles <myles.cillian@gmail.com>
2023-12-10 12:58:16 +0000

    03: part 1: wip

--- lib/2023/day_03.dart
+++ lib/2023/day_03.dart
@@ -9,8 +9,60 @@ void main() {
   print('Part 2: $answer2');
 }
 
+const _dot = '.';
+const _numbers = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+
+bool _isDot(String char) => char == _dot;
+
+bool _isNumber(String char) => _numbers.contains(char);
+
+bool _isSymbol(String char) => !_isDot(char) && !_isNumber(char);
+
 int part1(String input) {
-  return 0;
+  print(input);
+
+  final lines = input.split('\n');
+
+  var sum = 0;
+  for (int y = 0; y < lines.length; y++) {
+    final line = lines[y];
+    if (line.isEmpty) continue;
+
+    var value = '';
+    int start = 0;
+    int end = line.length - 1;
+
+    void reset() {
+      value = '';
+      start = 0;
+      end = line.length - 1;
+    }
+
+    void match() {
+      print('$value - start: $start - end: $end');
+      reset();
+    }
+
+    for (int x = 0; x < line.length; x++) {
+      final char = line[x];
+
+      if (x == 0) reset();
+
+      if (_isNumber(char)) {
+        if (value.isEmpty) start = x;
+        if (x == line.length - 1) {
+          end = x;
+          match();
+        }
+        value += char;
+      } else if (value.isNotEmpty) {
+        end = x;
+        match();
+      }
+    }
+  }
+
+  return sum;
 }
 
 int part2(String input) {
