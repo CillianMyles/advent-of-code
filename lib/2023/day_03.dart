65220948737b8e276d3def3b8cb00aecb03b8071
Cillian Myles <myles.cillian@gmail.com>
2023-12-10 11:48:18 +0000

    03: setup

---
+++ lib/2023/day_03.dart
@@ -0,0 +1,18 @@
+import 'dart:io';
+
+void main() {
+  final input = File('lib/2023/day_03.txt').readAsStringSync();
+  final answer1 = part1(input);
+  final answer2 = part2(input);
+  print('----------');
+  print('Part 1: $answer1');
+  print('Part 2: $answer2');
+}
+
+int part1(String input) {
+  return 0;
+}
+
+int part2(String input) {
+  return 0;
+}
---
+++ test/2023/day_03_test.dart
@@ -0,0 +1,53 @@
+import 'dart:io';
+
+import 'package:advent_of_code/2023/day_03.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('part 1', () {
+    test('sample', () {
+      expect(
+        part1(_sampleInput),
+        4361,
+      );
+    });
+
+    test('final test', () {
+      final input = File('lib/2023/day_03.txt').readAsStringSync();
+      expect(
+        part1(input),
+        42,
+      );
+    });
+  });
+
+  group('part 2', () {
+    test('sample', () {
+      expect(
+        part2(_sampleInput),
+        42,
+      );
+    });
+
+    test('final test', () {
+      final input = File('lib/2023/day_03.txt').readAsStringSync();
+      expect(
+        part2(input),
+        42,
+      );
+    });
+  });
+}
+
+const _sampleInput = r'''
+467..114..
+...*......
+..35..633.
+......#...
+617*......
+.....+.58.
+..592.....
+......755.
+...$.*....
+.664.598..
+''';
