dd091a762a927961445df6afbc77fde9dd887ac3
Cillian Myles <myles.cillian@gmail.com>
2024-12-02 09:40:11 +0000

    feat: day 01: improve dart

--- 2024/dart/lib/day_01.dart
+++ 2024/dart/lib/day_01.dart
@@ -2,51 +2,30 @@ import 'dart:io';
 
 void main() {
   final input = File('lib/day_01.txt').readAsStringSync();
+
   final distances = sumOfPairDistances(input);
   print('distances: $distances');
+
   final similarity = getSimilarityScore(input);
   print('similarity: $similarity');
 }
 
 int sumOfPairDistances(String input) {
   var sum = 0;
-  final lines = input.split('\n');
+  final (lhs, rhs) = _splitVertically(input);
 
-  final lhs = <int>[];
-  final rhs = <int>[];
-
-  for (final line in lines) {
-    if (line.isEmpty) continue;
-
-    final split = line.split('   ');
-    lhs.add(int.parse(split[0]));
-    rhs.add(int.parse(split[1]));
-  }
-
-  lhs.sort((a, b) => a.compareTo(b));
-  rhs.sort((a, b) => a.compareTo(b));
+  lhs.sort();
+  rhs.sort();
 
   for (int i = 0; i < lhs.length; i++) {
     sum += (lhs[i] - rhs[i]).abs();
   }
-
   return sum;
 }
 
 int getSimilarityScore(String input) {
   var similarity = 0;
-  final lines = input.split('\n');
-
-  final lhs = <int>[];
-  final rhs = <int>[];
-
-  for (final line in lines) {
-    if (line.isEmpty) continue;
-
-    final split = line.split('   ');
-    lhs.add(int.parse(split[0]));
-    rhs.add(int.parse(split[1]));
-  }
+  final (lhs, rhs) = _splitVertically(input);
 
   for (var l in lhs) {
     var occurrences = 0;
@@ -55,6 +34,20 @@ int getSimilarityScore(String input) {
     }
     similarity += l * occurrences;
   }
-
   return similarity;
+}
+
+(List<int> lhs, List<int> rhs) _splitVertically(String input) {
+  final lhs = <int>[];
+  final rhs = <int>[];
+
+  for (final line in input.split('\n')) {
+    if (line.isEmpty) break;
+
+    final split = line.split('   ');
+    lhs.add(int.parse(split[0]));
+    rhs.add(int.parse(split[1]));
+  }
+
+  return (lhs, rhs);
 }
