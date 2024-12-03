f7b305fa788a349246cfa03327b092d4382e3a56
Cillian Myles <myles.cillian@gmail.com>
2024-12-03 21:21:39 +0000

    feat: day 02: dart: part 01: remove debug logs

--- 2024/dart/lib/day_02.dart
+++ 2024/dart/lib/day_02.dart
@@ -13,7 +13,6 @@ void main() {
 int safeReportsCount(String input) {
   var count = 0;
   final lines = input.split('\n');
-  print('lines: [len=${lines.length}]: $lines');
 
   bool? ascending;
   for (var i = 0; i < lines.length; i++) {
@@ -21,26 +20,20 @@ int safeReportsCount(String input) {
     if (line.trim().isEmpty) break;
 
     final values = line.split(' ');
-    print('=' * 20);
-    print('values: [i=$i]: [len=${values.length}]: $values');
 
     for (var j = 0; j < values.length - 1; j++) {
       final current = int.parse(values[j]);
       final next = int.parse(values[j + 1]);
-      print('[j=$j]: current: $current, next: $next, ascending: $ascending');
+      ascending ??= next > current;
 
       if (current == next) {
-        print('break: current == next');
         ascending = null;
         break;
       }
 
-      ascending ??= (next > current);
-
       if (ascending) {
         final increasedBy = next - current;
         if (increasedBy < 1 || increasedBy > 3) {
-          print('break: increasedBy: $increasedBy');
           ascending = null;
           break;
         }
@@ -49,7 +42,6 @@ int safeReportsCount(String input) {
       if (!ascending) {
         final decreasedBy = current - next;
         if (decreasedBy < 1 || decreasedBy > 3) {
-          print('break: decreasedBy: $decreasedBy');
           ascending = null;
           break;
         }
@@ -57,8 +49,6 @@ int safeReportsCount(String input) {
 
       if (j == values.length - 2) {
         count++;
-        print(
-            'safe: [i=$i]: [count=$count]: [${ascending ? 'asc' : 'desc'}]: $line');
         ascending = null;
         break;
       }
