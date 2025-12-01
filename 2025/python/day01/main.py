9526ae1785f951741f05733e8c7d233ce7bd4212
Cillian Myles <cillian.myles@datavant.com>
2025-12-01 20:33:23 +0000

    feat: day 01

--- 2025/python/day01/main.py
+++ 2025/python/day01/main.py
@@ -4,7 +4,7 @@ from pathlib import Path
 _directory = Path(__file__).parent
 
 
-def determine_password(file: str, start: int = 50) -> int:
+def determine_password_part_1(file: str, start: int = 50) -> int:
     position = start
     zero_count = 0
 
@@ -38,12 +38,54 @@ def determine_password(file: str, start: int = 50) -> int:
 
 
 def part_1():
-    determine_password("p1-sample.input")
-    determine_password("p1-puzzle.input")
+    determine_password_part_1("p1-sample.input")
+    determine_password_part_1("p1-puzzle.input")
+
+
+def determine_password_part_2(file: str, start: int = 50) -> int:
+    position = start
+    zero_count = 0
+
+    with open(f"{_directory}/{file}", "r", encoding="utf-8") as file:
+        for line in file:
+            instruction = line.strip()
+            direction = instruction[0:1]
+            distance = int(instruction[1:])
+            starting_position = position
+
+            if direction == "L":
+                position = position - distance
+                while position < 0:
+                    position = position + 100
+                    if starting_position != 0 and position != 0:
+                        zero_count = zero_count + 1
+
+            elif direction == "R":
+                position = position + distance
+                while position > 99:
+                    position = position - 100
+                    if starting_position != 0 and position != 0:
+                        zero_count = zero_count + 1
+
+            else:
+                raise Exception(f"unexpected direction: {direction}")
+
+            if position == 0:
+                zero_count = zero_count + 1
+
+            print(f"{starting_position} - {instruction} - {position} - {zero_count}")
+
+    return zero_count
+
+
+def part_2():
+    determine_password_part_2("p1-sample.input")
+    # determine_password_part_2("p1-puzzle.input")
 
 
 def main():
-    part_1()
+    # part_1()
+    part_2()
 
 
 if __name__ == "__main__":
