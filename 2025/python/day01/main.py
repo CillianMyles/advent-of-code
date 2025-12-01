e8b16a68e923a75fe0ec42d6ba67976b822d3558
Cillian Myles <cillian.myles@datavant.com>
2025-12-01 20:03:58 +0000

    feat: day 01

--- 2025/python/day01/main.py
+++ 2025/python/day01/main.py
@@ -5,9 +5,40 @@ _directory = Path(__file__).parent
 
 
 def main():
+    position = 50
+    zero_count = 0
+
     with open(f"{_directory}/sample.input", "r", encoding="utf-8") as file:
         for line in file:
-            print(line.strip())
+            instruction = line.strip()
+            direction = instruction[0:1]
+            distance = int(instruction[1:])
+
+            if direction == "L":
+                if position - distance < 0:
+                    # overflow
+                    position = position - distance + 100
+                else:
+                    # normal
+                    position = position - distance
+
+            elif direction == "R":
+                if position + distance > 99:
+                    # overflow
+                    position = position + distance - 100
+                else:
+                    # normal
+                    position = position + distance
+
+            else:
+                raise Exception(f"unexpected direction: {direction}")
+
+            if position == 0:
+                zero_count = zero_count + 1
+
+            print(
+                f"{instruction} - {direction} - {distance} - {position} - {zero_count}"
+            )
 
 
 if __name__ == "__main__":
