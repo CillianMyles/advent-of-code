0ed0639dab5a8d8a3a86e34c97622136b0dfc1fa
Cillian Myles <cillian.myles@datavant.com>
2025-12-02 23:46:15 +0000

    feat: day 02

--- 2025/python/day02/main.py
+++ 2025/python/day02/main.py
@@ -20,23 +20,20 @@ def _sum_invalid_ids_part_1(file: str) -> int:
                 assert len(bounds) == 2, "expected range had 2 bounds"
                 lower = int(bounds[0])
                 upper = int(bounds[1])
-                for i in range(lower, upper):
-                    if i % 2 != 0:
+                for i in range(lower, upper + 1):
+                    number = str(i)
+                    length = len(number)
+                    if length % 2 != 0:
                         continue
-                    else:
-                        number = str(i)
-                        length = len(number)
-                        end_lhs = int((length / 2) - 1)
-                        start_rhs = end_lhs + 1
-                        lhs = number[: end_lhs + 1]
-                        rhs = number[start_rhs:]
-                        print(
-                            f"number: {number} - length: {length} - end_lhs: {end_lhs} - start_rhs: {start_rhs} - lhs: {lhs} - rhs: {rhs}"
-                        )
-                        for j in range(start_rhs):
-                            if lhs[j] != rhs[j]:
-                                continue
-                            invalid.append(i)
+                    end_lhs = int((length / 2) - 1)
+                    start_rhs = end_lhs + 1
+                    lhs = number[: end_lhs + 1]
+                    rhs = number[start_rhs:]
+                    if lhs == rhs:
+                        invalid.append(i)
+                    print(
+                        f"number: {number} - length: {length} - end_lhs: {end_lhs} - start_rhs: {start_rhs} - lhs: {lhs} - rhs: {rhs} - inv: {lhs == rhs}"
+                    )
 
     for value in invalid:
         sum += value
