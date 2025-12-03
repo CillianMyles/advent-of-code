6cf1fed4b362c66d52f02f6f19f36846e9cd3601
Cillian Myles <cillian.myles@datavant.com>
2025-12-03 11:05:37 +0000

    chore: day 02: refactor

--- 2025/python/day02/main.py
+++ 2025/python/day02/main.py
@@ -1,15 +1,14 @@
 from pathlib import Path
-from typing import List, Any
+from typing import List, Any, Tuple
 
 
 _directory = Path(__file__).parent
 
 
-def _sum_invalid_ids_part_1(file: str) -> int:
-    invalid = []
-    sum = 0
+def _load_ids(file: str) -> List[Tuple[int, int]]:
+    ids = []
 
-    with open(f"{_directory}/{file}", "r", encoding="utf-8") as f:
+    with open(file, "r", encoding="utf-8") as f:
         for line in f:
             line = line.strip()
             if not line:
@@ -20,29 +19,9 @@ def _sum_invalid_ids_part_1(file: str) -> int:
                 assert len(bounds) == 2, "expected range had 2 bounds"
                 lower = int(bounds[0])
                 upper = int(bounds[1])
-                for i in range(lower, upper + 1):
-                    number = str(i)
-                    length = len(number)
-                    if length % 2 != 0:
-                        continue
-                    end_lhs = int((length / 2) - 1)
-                    start_rhs = end_lhs + 1
-                    lhs = number[: end_lhs + 1]
-                    rhs = number[start_rhs:]
-                    if lhs == rhs:
-                        invalid.append(i)
+                ids.append((lower, upper))
 
-    for value in invalid:
-        sum += value
-
-    return sum
-
-
-def part_1():
-    sample = _sum_invalid_ids_part_1("p1-sample.input")
-    puzzle = _sum_invalid_ids_part_1("p1-puzzle.input")
-    print("Part 1 - Sample:", sample)
-    print("Part 1 - Puzzle:", puzzle)
+    return ids
 
 
 def _chunks(input, n) -> List[int]:
@@ -61,33 +40,57 @@ def _all_equal(items: List[Any]) -> bool:
     return True
 
 
-def _sum_invalid_ids_part_2(file: str) -> int:
+def sum_invalid_ids_part_1(file: str) -> int:
     invalid = []
-    sum = 0
 
-    with open(f"{_directory}/{file}", "r", encoding="utf-8") as f:
-        for line in f:
-            line = line.strip()
-            if not line:
+    id_ranges = _load_ids(f"{_directory}/{file}")
+    for id_range in id_ranges:
+        (lower, upper) = id_range
+        for i in range(lower, upper + 1):
+            number = str(i)
+            length = len(number)
+            if length % 2 != 0:
                 continue
-            id_ranges = line.split(",")
-            for id_range in id_ranges:
-                bounds = id_range.split("-")
-                assert len(bounds) == 2, "expected range had 2 bounds"
-                lower = int(bounds[0])
-                upper = int(bounds[1])
-                for i in range(lower, upper + 1):
-                    number = str(i)
-                    length = len(number)
-                    for j in range(1, length):
-                        if length % j != 0:
-                            continue
-                        items = _chunks(number, j)
-                        all_equal = _all_equal(items)
-                        if all_equal:
-                            invalid.append(i)
-                            break
+            end_lhs = int((length / 2) - 1)
+            start_rhs = end_lhs + 1
+            lhs = number[: end_lhs + 1]
+            rhs = number[start_rhs:]
+            if lhs == rhs:
+                invalid.append(i)
 
+    sum = 0
+    for value in invalid:
+        sum += value
+
+    return sum
+
+
+def part_1():
+    sample = sum_invalid_ids_part_1("p1-sample.input")
+    puzzle = sum_invalid_ids_part_1("p1-puzzle.input")
+    print("Part 1 - Sample:", sample)
+    print("Part 1 - Puzzle:", puzzle)
+
+
+def sum_invalid_ids_part_2(file: str) -> int:
+    invalid = []
+
+    id_ranges = _load_ids(f"{_directory}/{file}")
+    for id_range in id_ranges:
+        (lower, upper) = id_range
+        for i in range(lower, upper + 1):
+            number = str(i)
+            length = len(number)
+            for j in range(1, length):
+                if length % j != 0:
+                    continue
+                items = _chunks(number, j)
+                all_equal = _all_equal(items)
+                if all_equal:
+                    invalid.append(i)
+                    break
+
+    sum = 0
     for value in invalid:
         sum += value
 
@@ -95,8 +98,8 @@ def _sum_invalid_ids_part_2(file: str) -> int:
 
 
 def part_2():
-    sample = _sum_invalid_ids_part_2("p1-sample.input")
-    puzzle = _sum_invalid_ids_part_2("p1-puzzle.input")
+    sample = sum_invalid_ids_part_2("p1-sample.input")
+    puzzle = sum_invalid_ids_part_2("p1-puzzle.input")
     print("Part 2 - Sample:", sample)
     print("Part 2 - Puzzle:", puzzle)
 
