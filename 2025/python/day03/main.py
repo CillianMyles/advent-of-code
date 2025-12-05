62440c263a8cb7fec2cf053d20b9a1cf42c234d2
Cillian Myles <myles.cillian@gmail.com>
2025-12-05 08:51:29 +0000

    feat: day 03

---
+++ 2025/python/day03/main.py
@@ -0,0 +1,48 @@
+from pathlib import Path
+from typing import List, Any, Tuple, Iterable
+
+
+_directory = Path(__file__).parent
+
+
+def _read_lines(filename: str) -> Iterable[str]:
+    filepath = _directory / filename
+    with open(filepath, "r", encoding="utf-8") as f:
+        for line in f:
+            line = line.strip()
+            if not line:
+                continue
+            yield line
+
+
+def calculate_part_1(filename: str) -> int:
+    sum = 0
+    return sum
+
+
+def calculate_part_2(filename: str) -> int:
+    sum = 0
+    return sum
+
+
+def part_1():
+    sample = calculate_part_1("p1-sample.input")
+    puzzle = calculate_part_1("p1-puzzle.input")
+    print("Part 1 - Sample:", sample)
+    print("Part 1 - Puzzle:", puzzle)
+
+
+def part_2():
+    sample = calculate_part_2("p1-sample.input")
+    puzzle = calculate_part_2("p1-puzzle.input")
+    print("Part 2 - Sample:", sample)
+    print("Part 2 - Puzzle:", puzzle)
+
+
+def main():
+    part_1()
+    part_2()
+
+
+if __name__ == "__main__":
+    main()
---
+++ 2025/python/day03/test_main.py
@@ -0,0 +1,25 @@
+from main import calculate_part_1, calculate_part_2
+
+
+def test_part_1_sample_input():
+    input = "p1-sample.input"
+    password = calculate_part_1(input)
+    assert password == -1
+
+
+def test_part_1_puzzle_input():
+    input = "p1-puzzle.input"
+    password = calculate_part_1(input)
+    assert password == -1
+
+
+def test_part_2_sample_input():
+    input = "p1-sample.input"
+    password = calculate_part_2(input)
+    assert password == -1
+
+
+def test_part_2_puzzle_input():
+    input = "p1-puzzle.input"
+    password = calculate_part_2(input)
+    assert password == -1
