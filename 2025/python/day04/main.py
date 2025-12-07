c02ea3dc82ff4cc3e028eb3fafaf85fecfde7ec0
Cillian Myles <myles.cillian@gmail.com>
2025-12-07 20:20:57 +0000

    feat: day 04

--- 2025/python/day04/main.py
+++ 2025/python/day04/main.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from typing import Iterable
+from typing import Iterable, List
 
 
 _directory = Path(__file__).parent
@@ -16,7 +16,27 @@ def _read_lines(filename: str) -> Iterable[str]:
 
 
 def calculate_part_1(filename: str) -> int:
-    return 0
+    total = 0
+
+    num_rows = 0
+    row_length = None
+    matrix: List[List[str]] = []
+    for line in _read_lines(filename):
+        row = line.split()
+        if row_length is None:
+            row_length = len(row)
+        else:
+            assert row_length == len(row), "unexpected row length"
+        matrix.append(row)
+        num_rows += 1
+    assert num_rows, "should be a non-zero number of rows"
+    assert row_length, "rows should have a non-zero length"
+
+    for y in range(num_rows):
+        for x in range(row_length):
+            print(f"x={x} y={y}")
+
+    return total
 
 
 def calculate_part_2(filename: str) -> int:
@@ -25,9 +45,9 @@ def calculate_part_2(filename: str) -> int:
 
 def part_1():
     sample = calculate_part_1("p1-sample.input")
-    puzzle = calculate_part_1("p1-puzzle.input")
+    # puzzle = calculate_part_1("p1-puzzle.input")
     print("Part 1 - Sample:", sample)
-    print("Part 1 - Puzzle:", puzzle)
+    # print("Part 1 - Puzzle:", puzzle)
 
 
 def part_2():
@@ -39,7 +59,7 @@ def part_2():
 
 def main():
     part_1()
-    part_2()
+    # part_2()
 
 
 if __name__ == "__main__":
