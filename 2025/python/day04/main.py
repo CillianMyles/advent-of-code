e53ba1db549d2b4e9c81ab401ee17ed652954e71
Cillian Myles <myles.cillian@gmail.com>
2025-12-09 09:27:11 +0000

    feat: day 04

--- 2025/python/day04/main.py
+++ 2025/python/day04/main.py
@@ -19,24 +19,28 @@ def calculate_part_1(filename: str) -> int:
     total = 0
 
     num_rows = 0
-    row_length = None
+    num_columns = None
     matrix: List[List[str]] = []
     for line in _read_lines(filename):
         row = list(line)
-        if row_length is None:
-            row_length = len(row)
+        if num_columns is None:
+            num_columns = len(row)
         else:
-            assert row_length == len(row), "unexpected row length"
+            assert num_columns == len(row), "unexpected row length"
         matrix.append(row)
         num_rows += 1
-        print(f"line: {line} - row: {row} - len: {row_length}")
+        print(f"line: {line} - row: {row} - len: {num_columns}")
     assert num_rows, "should be a non-zero number of rows"
-    assert row_length, "rows should have a non-zero length"
+    assert num_columns, "should be a non-zero number of columns"
 
-    range_x = range(row_length - 1)
-    range_y = range(num_rows - 1)
+    max_x = num_columns
+    max_y = num_rows
+
+    def in_bounds(x, y):
+        return 0 <= x < max_x and 0 <= y < max_y
+
     for y in range(num_rows):
-        for x in range(row_length):
+        for x in range(num_columns):
             adjascent = 0
 
             top_middle = (x, y - 1)
@@ -44,62 +48,54 @@ def calculate_part_1(filename: str) -> int:
             centre_right = (x + 1, y)
             bottom_right = (x + 1, y + 1)
             bottom_middle = (x, y + 1)
-            bottom_left = (x - 1, x + 1)
+            bottom_left = (x - 1, y + 1)
             centre_left = (x - 1, y)
             top_left = (x - 1, y - 1)
 
             if (
-                top_middle[0] in range_x
-                and top_middle[1] in range_y
+                in_bounds(*top_middle)
                 and matrix[top_middle[1]][top_middle[0]] == "@"
             ):
                 adjascent += 1
                 print(f"[x:{x}, y:{y}] - top_middle: {top_middle}: ✅")
             if (
-                top_right[0] in range_x
-                and top_right[1] in range_y
+                in_bounds(*top_right)
                 and matrix[top_right[1]][top_right[0]] == "@"
             ):
                 adjascent += 1
                 print(f"[x:{x}, y:{y}] - top_right: {top_right}: ✅")
             if (
-                centre_right[0] in range_x
-                and centre_right[1] in range_y
+                in_bounds(*centre_right)
                 and matrix[centre_right[1]][centre_right[0]] == "@"
             ):
                 adjascent += 1
                 print(f"[x:{x}, y:{y}] - centre_right: {centre_right}: ✅")
             if (
-                bottom_right[0] in range_x
-                and bottom_right[1] in range_y
+                in_bounds(*bottom_right)
                 and matrix[bottom_right[1]][bottom_right[0]] == "@"
             ):
                 adjascent += 1
                 print(f"[x:{x}, y:{y}] - bottom_right: {bottom_right}: ✅")
             if (
-                bottom_middle[0] in range_x
-                and bottom_middle[1] in range_y
+                in_bounds(*bottom_middle)
                 and matrix[bottom_middle[1]][bottom_middle[0]] == "@"
             ):
                 adjascent += 1
                 print(f"[x:{x}, y:{y}] - bottom_middle: {bottom_middle}: ✅")
             if (
-                bottom_left[0] in range_x
-                and bottom_left[1] in range_y
+                in_bounds(*bottom_left)
                 and matrix[bottom_left[1]][bottom_left[0]] == "@"
             ):
                 adjascent += 1
                 print(f"[x:{x}, y:{y}] - bottom_left: {bottom_left}: ✅")
             if (
-                centre_left[0] in range_x
-                and centre_left[1] in range_y
+                in_bounds(*centre_left)
                 and matrix[centre_left[1]][centre_left[0]] == "@"
             ):
                 adjascent += 1
                 print(f"[x:{x}, y:{y}] - centre_left: {centre_left}: ✅")
             if (
-                top_left[0] in range_x
-                and top_left[1] in range_y
+                in_bounds(*top_left)
                 and matrix[top_left[1]][top_left[0]] == "@"
             ):
                 adjascent += 1
