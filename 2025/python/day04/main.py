0acd56171d8899844387311d267b69e04db28c4f
Cillian Myles <myles.cillian@gmail.com>
2025-12-10 21:57:44 +0000

    feat: day 04: refactor

--- 2025/python/day04/main.py
+++ 2025/python/day04/main.py
@@ -18,92 +18,50 @@ def _read_lines(filename: str) -> Iterable[str]:
 def calculate_part_1(filename: str) -> int:
     total = 0
 
-    num_rows = 0
-    num_columns = None
-    matrix: List[List[str]] = []
+    data: List[List[str]] = []
     for line in _read_lines(filename):
         row = list(line)
-        if num_columns is None:
-            num_columns = len(row)
-        else:
-            assert num_columns == len(row), "unexpected row length"
-        matrix.append(row)
-        num_rows += 1
-        print(f"line: {line} - row: {row} - len: {num_columns}")
-    assert num_rows, "should be a non-zero number of rows"
-    assert num_columns, "should be a non-zero number of columns"
+        data.append(row)
 
-    max_x = num_columns
-    max_y = num_rows
-
-    def in_bounds(x, y):
-        return 0 <= x < max_x and 0 <= y < max_y
-
-    for y in range(num_rows):
-        for x in range(num_columns):
+    grid = Grid(data)
+    for row in range(grid.num_rows):
+        for col in range(grid.num_cols):
             adjascent = 0
+            point = Point(row, col)
+            print(f"======\n{point!r}")
 
-            top_middle = (x, y - 1)
-            top_right = (x + 1, y - 1)
-            centre_right = (x + 1, y)
-            bottom_right = (x + 1, y + 1)
-            bottom_middle = (x, y + 1)
-            bottom_left = (x - 1, y + 1)
-            centre_left = (x - 1, y)
-            top_left = (x - 1, y - 1)
-
-            if (
-                in_bounds(*top_middle)
-                and matrix[top_middle[1]][top_middle[0]] == "@"
-            ):
+            if grid.value_at(point.top_middle) == "@":
                 adjascent += 1
-                print(f"[x:{x}, y:{y}] - top_middle: {top_middle}: ✅")
-            if (
-                in_bounds(*top_right)
-                and matrix[top_right[1]][top_right[0]] == "@"
-            ):
+                print(f"top_middle: {point.top_middle!r}: ☑️")
+            if grid.value_at(point.top_right) == "@":
                 adjascent += 1
-                print(f"[x:{x}, y:{y}] - top_right: {top_right}: ✅")
-            if (
-                in_bounds(*centre_right)
-                and matrix[centre_right[1]][centre_right[0]] == "@"
-            ):
+                print(f"top_right: {point.top_right!r}: ☑️")
+            if grid.value_at(point.centre_right) == "@":
                 adjascent += 1
-                print(f"[x:{x}, y:{y}] - centre_right: {centre_right}: ✅")
-            if (
-                in_bounds(*bottom_right)
-                and matrix[bottom_right[1]][bottom_right[0]] == "@"
-            ):
+                print(f"centre_right: {point.centre_right!r}: ☑️")
+            if grid.value_at(point.bottom_right) == "@":
                 adjascent += 1
-                print(f"[x:{x}, y:{y}] - bottom_right: {bottom_right}: ✅")
-            if (
-                in_bounds(*bottom_middle)
-                and matrix[bottom_middle[1]][bottom_middle[0]] == "@"
-            ):
+                print(f"bottom_right: {point.bottom_right!r}: ☑️")
+            if grid.value_at(point.bottom_middle) == "@":
                 adjascent += 1
-                print(f"[x:{x}, y:{y}] - bottom_middle: {bottom_middle}: ✅")
-            if (
-                in_bounds(*bottom_left)
-                and matrix[bottom_left[1]][bottom_left[0]] == "@"
-            ):
+                print(f"bottom_middle: {point.bottom_middle!r}: ☑️")
+            if grid.value_at(point.bottom_left) == "@":
                 adjascent += 1
-                print(f"[x:{x}, y:{y}] - bottom_left: {bottom_left}: ✅")
-            if (
-                in_bounds(*centre_left)
-                and matrix[centre_left[1]][centre_left[0]] == "@"
-            ):
+                print(f"bottom_left: {point.bottom_left!r}: ☑️")
+            if grid.value_at(point.centre_left) == "@":
                 adjascent += 1
-                print(f"[x:{x}, y:{y}] - centre_left: {centre_left}: ✅")
-            if (
-                in_bounds(*top_left)
-                and matrix[top_left[1]][top_left[0]] == "@"
-            ):
+                print(f"centre_left: {point.centre_left!r}: ☑️")
+            if grid.value_at(point.top_left) == "@":
                 adjascent += 1
-                print(f"[x:{x}, y:{y}] - top_left: {top_left}: ✅")
+                print(f"top_left: {point.top_left!r}: ☑️")
 
             if adjascent < 4:
                 total += 1
+                print(f"{point!r}: ✅")
+            else:
+                print(f"{point!r}: ❌")
 
+    print(f"======")
     return total
 
 
@@ -112,63 +70,71 @@ def calculate_part_2(filename: str) -> int:
 
 
 class Point:
-    def __init__(self, row: int, column: int):
-        self._row = row
-        self._column = column
+    def __init__(self, row: int, col: int):
+        self.row = row
+        self.col = col
 
-    def r(self) -> int:
-        return self._row
+    @property
+    def top_middle(self) -> Point:
+        return Point(self.row - 1, self.col)
 
-    def c(self) -> int:
-        return self._column
+    @property
+    def top_right(self) -> Point:
+        return Point(self.row - 1, self.col + 1)
 
-    def top_middle(self):
-        return Point(self.r - 1, self.c)
+    @property
+    def centre_right(self) -> Point:
+        return Point(self.row, self.col + 1)
 
-    def top_right(self):
-        return Point(self.r - 1, self.c + 1)
+    @property
+    def bottom_right(self) -> Point:
+        return Point(self.row + 1, self.col + 1)
 
-    def centre_right(self):
-        return Point(self.r, self.c + 1)
+    @property
+    def bottom_middle(self) -> Point:
+        return Point(self.row + 1, self.col)
 
-    def bottom_right(self):
-        return Point(self.r + 1, self.c + 1)
+    @property
+    def bottom_left(self) -> Point:
+        return Point(self.row + 1, self.col - 1)
 
-    def bottom_middle(self):
-        return Point(self.r + 1, self.c)
+    @property
+    def centre_left(self) -> Point:
+        return Point(self.row, self.col - 1)
 
-    def bottom_left(self):
-        return Point(self.r + 1, self.c - 1)
+    @property
+    def top_left(self) -> Point:
+        return Point(self.row - 1, self.col - 1)
 
-    def centre_left(self):
-        return Point(self.r, self.c - 1)
+    def __str__(self) -> str:
+        return f"Point(row: {self.row}, col: {self.col})"
 
-    def top_left(self):
-        return Point(self.r - 1, self.c - 1)
-
-    def __str__(self):
-        return f"Point(row: {self.r}, column: {self.c})"
+    def __repr__(self) -> str:
+        return f"[r:{self.row}, c:{self.col}]"
 
 
 class Grid:
     def __init__(self, data: List[List[str]]):
         self._data = data
-        self._num_rows = len(data)
-        self._num_cols: int | None = None
+        self.num_rows = len(data)
+        self.num_cols: int | None = None
         for row in data:
-            for column in row:
-                if self._num_cols is None:
-                    self._num_cols = len(column)
-                else:
-                    assert self._num_cols == len(column)
+            if self.num_cols is None:
+                self.num_cols = len(row)
+            else:
+                assert self.num_cols == len(row), "unexpected row length"
+        assert self.num_rows, "expected non-zero number of rows"
+        assert self.num_cols, "expected non-zero number of columns"
 
     def in_bounds(self, point: Point) -> bool:
-        return 0 <= point.r < self._num_rows and 0 <= point.c < self._num_cols
+        valid_row = 0 <= point.row < self.num_rows
+        valid_col = 0 <= point.col < self.num_cols
+        return valid_row and valid_col
 
     def value_at(self, point: Point) -> str | None:
         if not self.in_bounds(point):
             return None
-        return self._data[point.c][point.r]
+        return self._data[point.col][point.row]
 
 
 def part_1():
