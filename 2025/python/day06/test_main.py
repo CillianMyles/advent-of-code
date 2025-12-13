ef57d37dced281eed8fbf66574876b1ccc71290d
Cillian Myles <myles.cillian@gmail.com>
2025-12-13 19:41:37 +0000

    feat: day 06: skeleton

--- 2025/python/day06/test_main.py
+++ 2025/python/day06/test_main.py
@@ -4,7 +4,7 @@ from main import calculate_part_1, calculate_part_2
 def test_part_1_sample_input():
     input = "p1-sample.input"
     result = calculate_part_1(input)
-    assert result == -1
+    assert result == 4277556
 
 
 def test_part_1_puzzle_input():
