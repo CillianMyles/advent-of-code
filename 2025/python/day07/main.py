44615f0c860964874476da557838e04cd2a81c07
Cillian Myles <myles.cillian@gmail.com>
2025-12-17 20:19:01 +0000

    feat: day 7: part 1: wip

--- 2025/python/day07/main.py
+++ 2025/python/day07/main.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 from math import prod
 from pathlib import Path
-from typing import Iterable, Iterator
+from typing import Iterable, Iterator, List
 
 _directory = Path(__file__).parent
 
@@ -11,12 +11,49 @@ def _read_lines(filename: str) -> Iterator[str]:
     filepath = _directory / filename
     with filepath.open("r", encoding="utf-8") as file:
         for line in file:
-            line = line.rstrip("\n")
+            line = line.strip()
             if line:
                 yield line
 
 
 def part_1(filename: str) -> int:
+    grid = [list(line) for line in _read_lines(filename)]
+
+    start: int | None = None
+    beams = [[] for _ in grid]
+
+    for i in range(len(grid)):
+        curr = grid[i]
+
+        if i == 0:
+            start = curr.index("S")
+            print(f'[0] "S" found at {start}')
+            continue
+
+        if i == 1:
+            if curr[start] == ".":
+                grid[i][start] = "|"
+                continue
+            else:
+                raise Exception("expected (.) below (S)")
+
+        prev = grid[i - 1]
+        markers = [i for i, v in enumerate(curr) if v == "^"]
+        beams = [i for i, v in enumerate(prev) if v == "|"]
+        print(f"[{i}] - beams (|): {beams} - markers (^): {markers}")
+
+        # if i == len(grid) - 1:
+        #     print("TODO")
+        #     continue
+
+        # next = grid[i+1]
+
+        # target = 'S' if i == 1 else '|'
+
+        # chars = list(line)
+        # grid.insert(i, chars)
+        # print(f"[{i}] chars: {chars}")
+    # print(grid)
     return 0
 
 
--- 2025/python/day07/test_main.py
+++ 2025/python/day07/test_main.py
@@ -4,7 +4,7 @@ from main import part_1, part_2
 def test_part_1_sample_input():
     input = "p1-sample.input"
     result = part_1(input)
-    assert result == -1
+    assert result == 21
 
 
 def test_part_1_puzzle_input():
