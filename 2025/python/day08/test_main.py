26c808527be064a6c639a9b0dbbaeeb839f2a42e
Cillian Myles <cillian.myles@datavant.com>
2026-01-02 16:31:28 +0000

    feat: day 8: part 1: prep

---
+++ 2025/python/day08/test_main.py
@@ -0,0 +1,25 @@
+from main import part_1, part_2
+
+
+def test_part_1_sample_input():
+    input = "p1-sample.input"
+    result = part_1(input)
+    assert result == -1
+
+
+def test_part_1_puzzle_input():
+    input = "p1-puzzle.input"
+    result = part_1(input)
+    assert result == -1
+
+
+def test_part_2_sample_input():
+    input = "p1-sample.input"
+    result = part_2(input)
+    assert result == -1
+
+
+def test_part_2_puzzle_input():
+    input = "p1-puzzle.input"
+    result = part_2(input)
+    assert result == -1
